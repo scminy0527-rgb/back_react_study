2798ff34514a21b4d7beed9b1af5a3868f9eabdd
scminy0527-rgb <scminy0527@gmail.com>
2026-02-08 15:24:46 +0900

    feat: useInput 설명

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+// 사용자가 무엇인 가를 입력하기 위해서는 아래와 같은 사항이 계속 필요함
+// 그런데 해당 과정을 함수로 묶어서 간편하게 해주는 개념
+
+// 초기값: 처음에 둘 디폴트 값
+// 리턴값: 초기값 및 해당 값을 변경 하는 함수 정의
 const useInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
   const onChangeValue = (e) => {
